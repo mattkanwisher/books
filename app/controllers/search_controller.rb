a93abb3a5272c3ad825d5ee00ee032c06e5c5fb2
Matthew Campbell <hyper@hyperworks.nu>
2008-08-09 16:35:05 -0400

    books for email

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -74,7 +74,8 @@ class SearchController < ApplicationController
     
     book = Book.find_by_title(title) || Book.new
     book.asin = asin
-    book.title = slightly_nicer_title(title)
+#    book.title = slightly_nicer_title(title)
+    book.title = title
     book.image_url = image_url
     book.author = author
     book.amz_purchase_url = page_url
